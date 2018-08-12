b6a8bc4a09252ec4a04a4174720fde1b526a71a7
Molly Drumm <m.drumm89@googlemail.com>
2018-08-12 22:56:22 +0100

    add Shop class

---
+++ src/main/java/shops/Shop.java
@@ -0,0 +1,19 @@
+package shops;
+
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
+public class Shop {
+    private ArrayList<ISell> stock;
+
+    public Shop(ArrayList<ISell> stock) {
+        this.stock = stock;
+
+    }
+    
+
+    public ArrayList<ISell> stock() {
+        return stock;
+    }
+}
